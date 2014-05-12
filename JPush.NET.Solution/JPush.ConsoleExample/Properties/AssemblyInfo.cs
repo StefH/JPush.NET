1e680d4b9f821a45c2ee77044aaa940ef25ce6a6
Stef Heyenrath <stef.heyenrath@gmail.com>
2014-05-12 20:13:43 +0200

    .
    .

--- JPush.NET.Solution/JPush.ConsoleExample/Properties/AssemblyInfo.cs
+++ JPush.NET.Solution/JPush.ConsoleExample/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JPushBrokerConsoleExample")]
+[assembly: AssemblyTitle("JPushConsoleExample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JPushBrokerConsoleExample")]
+[assembly: AssemblyProduct("JPushConsoleExample")]
 [assembly: AssemblyCopyright("Copyright © Stef Heyenrath 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
