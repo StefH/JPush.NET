7e8c001a09b0c284b0438a9c82fbe357b4e562e0
Stef <Stef.Heyenrath@gmail.com>
2014-05-12 10:52:06 +0200

    SendBroadcastMessage & SendByRegistrationIdMessage
    Added SendBroadcastMessage() & SendByRegistrationIdMessage() methods.

--- JPush.NET.Solution/JPush.ConsoleExample/Program.cs
+++ JPush.NET.Solution/JPush.ConsoleExample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using JPush;
 using JPush.Models;
 
@@ -17,13 +18,78 @@ namespace JPushConsoleExample
         {
             ReadIniFile();
 
-            var customizedValues = new Dictionary<string, string>();
-            customizedValues.Add("CK1", "CV1");
-            customizedValues.Add("CK2", "CV2");
-
             var client = new JPushClient(appKey, masterSecret, false);
             //client.OverrideApiUrl("http://localhost", 10000);
             //client.OverrideReportUrl("http://localhost", 10001);
+
+            SendBroadcastMessage(client);
+            SendByRegistrationIdMessage(client);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.Read();
+        }
+
+        private static void SendByRegistrationIdMessage(JPushClient client)
+        {
+            var request = new PushMessageRequest
+           {
+               MessageType = MessageType.Notification,
+               Platform = PushPlatform.Android,
+               Description = "DotNET",
+               PushType = PushType.ByRegistrationId,
+               ReceiverValue = "040f2c4faa0",
+               IsTestEnvironment = true,
+               Message = new PushMessage
+               {
+                   Content = "JPush ByRegistrationId message @ " + DateTime.Now.ToLongTimeString(),
+                   PushTitle = "ASYNC : <推送服务>",
+                   Sound = "YourSound"
+               }
+           };
+
+            var idToCheck = new List<string>();
+            var sendTask = client.SendPushMessageAsync(request);
+            sendTask.ContinueWith((task) =>
+            {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("IsFaulted : " + task.Exception);
+                }
+                else if (task.IsCanceled)
+                {
+                    Console.WriteLine("IsCanceled");
+                }
+                else
+                {
+                    Console.WriteLine("SendPushMessageAsync : " + task.Result.ResponseCode + ":" + task.Result.ResponseMessage);
+                    Console.WriteLine("Push sent : " + task.Result.MessageId);
+
+                    idToCheck.Add(task.Result.MessageId);
+
+
+                }
+            })
+            .Wait(TimeSpan.FromSeconds(3))
+            ;
+
+            var statusList = client.QueryPushMessageStatus(idToCheck);
+            Console.WriteLine("QueryPushMessageStatus is completed.");
+
+            if (statusList != null)
+            {
+                foreach (var status in statusList)
+                {
+                    Console.WriteLine("Id: {0}, Android: {1}, iOS: {2}", status.MessageId, status.AndroidDeliveredCount,
+                        status.ApplePushNotificationDeliveredCount);
+                }
+            }
+        }
+
+        private static void SendBroadcastMessage(JPushClient client)
+        {
+            var customizedValues = new Dictionary<string, string>();
+            customizedValues.Add("CK1", "CV1");
+            customizedValues.Add("CK2", "CV2");
 
             var request = new PushMessageRequest
             {
@@ -47,7 +113,6 @@ namespace JPushConsoleExample
             Console.WriteLine("SendPushMessage : " + response.ResponseCode + ":" + response.ResponseMessage);
             Console.WriteLine("Push sent : " + response.MessageId);
 
-
             request.Message.PushTitle += "ASYNC";
             var sendTask = client.SendPushMessageAsync(request);
             sendTask.ContinueWith((task) =>
@@ -65,11 +130,11 @@ namespace JPushConsoleExample
                     Console.WriteLine("SendPushMessageAsync : " + task.Result.ResponseCode + ":" + task.Result.ResponseMessage);
                     Console.WriteLine("Push sent : " + task.Result.MessageId);
 
-                    idToCheck.Add(response.MessageId);
+                    idToCheck.Add(task.Result.MessageId);
                 }
-            });
-
-            sendTask.Wait(TimeSpan.FromSeconds(1));
+            })
+            .Wait(TimeSpan.FromSeconds(3))
+            ;
 
             idToCheck.Add(response.MessageId);
 
@@ -80,12 +145,10 @@ namespace JPushConsoleExample
             {
                 foreach (var status in statusList)
                 {
-                    Console.WriteLine("Id: {0}, Android: {1}, iOS: {2}", status.MessageId, status.AndroidDeliveredCount, status.ApplePushNotificationDeliveredCount);
+                    Console.WriteLine("Id: {0}, Android: {1}, iOS: {2}", status.MessageId, status.AndroidDeliveredCount,
+                        status.ApplePushNotificationDeliveredCount);
                 }
             }
-
-            Console.WriteLine("Press any key to exit.");
-            Console.Read();
         }
 
         private static void ReadIniFile()
